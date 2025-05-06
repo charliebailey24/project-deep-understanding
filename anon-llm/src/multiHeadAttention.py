a91f0fa7b859c66072a033fca5a9fccc90577528
charliebailey24 <charliebailey24@gmail.com>
2025-05-05 22:48:54 -0700

    updated multi-head attention wrapper

---
+++ anon-llm/src/multiHeadAttention.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from tiktoken._educational import *
+from preprocessing import *
+from trainableAttention import *
+from causalAttention import *
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention_v2(
+                 d_in, d_out, context_length, dropout, qkv_bias
+             ) 
+             for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
